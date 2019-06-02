1174952f1ac6d6dddc23c6a895b795c80fdb857d
palto42 <12280188+palto42@users.noreply.github.com>
2019-06-02 20:09:29 +0200

    added bitnum option

--- RPi_utils/codesend.cpp
+++ RPi_utils/codesend.cpp
@@ -5,37 +5,43 @@ protocol    - According to rc-switch definitions
 pulselength - pulselength in microseconds
 
  'codesend' hacked from 'send' by @justy
- 
+
  - The provided rc_switch 'send' command uses the form systemCode, unitCode, command
-   which is not suitable for our purposes.  Instead, we call 
+   which is not suitable for our purposes.  Instead, we call
    send(code, length); // where length is always 24 and code is simply the code
    we find using the RF_sniffer.ino Arduino sketch.
 
-(Use RF_Sniffer.ino to check that RF signals are being produced by the RPi's transmitter 
+(Use RF_Sniffer.ino to check that RF signals are being produced by the RPi's transmitter
 or your remote control)
+
+Changes by palto42:
+  * Added command line option for code length
 */
+
 #include "../rc-switch/RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
-     
+
 
 int main(int argc, char *argv[]) {
-    
+
     // This pin is not the first pin on the RPi GPIO header!
     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
     // for more information.
     int PIN = 0;
-    
+
     // Parse the first parameter to this command as an integer
     int protocol = 0; // A value of 0 will use rc-switch's default value
     int pulseLength = 0;
+    int bitNum = 24;
 
     // If no command line argument is given, print the help text
     if (argc == 1) {
-        printf("Usage: %s decimalcode [protocol] [pulselength]\n", argv[0]);
+        printf("Usage: %s decimalcode [protocol] [pulselength] [bitnum]\n", argv[0]);
         printf("decimalcode\t- As decoded by RFSniffer\n");
         printf("protocol\t- According to rc-switch definitions\n");
         printf("pulselength\t- pulselength in microseconds\n");
+        printf("bitnum\t- message lenth in bits (default 24)\n");
         return -1;
     }
 
@@ -43,16 +49,17 @@ int main(int argc, char *argv[]) {
     int code = atoi(argv[1]);
     if (argc >= 3) protocol = atoi(argv[2]);
     if (argc >= 4) pulseLength = atoi(argv[3]);
-    
+    if (argc >= 5) bitNum = atoi(argv[4]);
+
     if (wiringPiSetup () == -1) return 1;
     printf("sending code[%i]\n", code);
     RCSwitch mySwitch = RCSwitch();
     if (protocol != 0) mySwitch.setProtocol(protocol);
     if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
     mySwitch.enableTransmit(PIN);
-    
-    mySwitch.send(code, 24);
-    
+
+    mySwitch.send(code, bitNum);
+
     return 0;
 
 }
